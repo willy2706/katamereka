cdcdcf2972dfa4e57104e464bae63f190a960688
willy2706 <willylie2706@gmail.com>
2015-05-28 00:39:21 +0700

    smoothscroll

--- js/custom.js
+++ js/custom.js
@@ -303,4 +303,20 @@ $(document).ready(function() {
 	});
 
 });
+
+$(function() {
+  $('.gotocontact').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 			
